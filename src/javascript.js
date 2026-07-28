008c0e8628ee693cea572d7c1e295fb9f2821cc4
Miracle <miiracle.sekome18@gmail.com>
2026-07-28 14:06:39 +0200

    Added javascript.js file and id's

---
+++ src/javascript.js
@@ -0,0 +1,8 @@
+function searchEngine (event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchEngine);
