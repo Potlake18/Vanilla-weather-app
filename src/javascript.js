bcda7b78e6c0ead9e143ebb185f1238ba24f384c
Miracle <miiracle.sekome18@gmail.com>
2026-08-03 14:00:58 +0200

    Added an axios function

--- src/javascript.js
+++ src/javascript.js
@@ -1,9 +1,14 @@
-
-function searchEngine (event) {
+function searchCity(city) {
+    let apiKey = "0aeta601556c13caob443ba54543f7b0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+}
+function searchEngine(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchEngine);
