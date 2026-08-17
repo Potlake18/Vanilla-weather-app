49540e7c7578a1c04b3b6bc916b5fae906e396c2
Miracle <miiracle.sekome18@gmail.com>
2026-08-17 13:04:17 +0200

    Included a weather forecast and implemented the API integration

--- src/javascript.js
+++ src/javascript.js
@@ -49,26 +49,36 @@ function getForecast(city) {
     axios(apiUrl).then(displayForecast);
 }
 
-function displayForecast() {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
+function displayForecast(response) {
     let forecastHtml = "";
 
-    days.forEach(function (day) {
-        forecastHtml =
-            forecastHtml +
-            `
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml =
+                forecastHtml +
+                `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src ="${day.condition.icon_url}" class= "weather-forecast-icon"/>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+                    day.temperature.minimum)}º</div>
         </div>
       </div>
     `;
+        }
     });
+
 
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
@@ -76,3 +86,5 @@ function displayForecast() {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchEngine);
+
+searchCity("Johannesburg");
